88c76f6449c45b25cabe0c12fa980a042f645a46
nriviere <nriviere@student.42.fr>
2022-11-29 12:54:15 +0100

    norminette cleared

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,10 +6,9 @@
 /*   By: nriviere <nriviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 18:07:36 by nriviere          #+#    #+#             */
-/*   Updated: 2022/11/29 12:48:45 by nriviere         ###   ########.fr       */
+/*   Updated: 2022/11/29 12:53:36 by nriviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 char	*ft_strchr(const char *s, int c)
 {
